f4766e16d1395caf694f5ac342b6ab2f51fffb97
Fábio M. Costa <fmc@ufg.br>
2026-04-08 08:39:02 -0300

    Update client2.py

--- client2.py
+++ client2.py
@@ -3,8 +3,8 @@ import Demo
  
 communicator = Ice.initialize(sys.argv)
 
-base1 = communicator.stringToProxy("SimplePrinter1:tcp 98.90.53.6 -p 11000")
-base2 = communicator.stringToProxy("SimplePrinter2:tcp 98.90.53.6 -p 11000")
+base1 = communicator.stringToProxy("SimplePrinter1:tcp -h 98.90.53.6 -p 11000")
+base2 = communicator.stringToProxy("SimplePrinter2:tcp -h 98.90.53.6 -p 11000")
 printer1 = Demo.PrinterPrx.checkedCast(base1)
 printer2 = Demo.PrinterPrx.checkedCast(base2)
 if (not printer1) or (not printer2):
