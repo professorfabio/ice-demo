70fad0f4f721d27f78e5e3ab72133cb5e8ae997b
Fábio M. Costa <fmc@ufg.br>
2026-04-10 18:16:30 -0300

    Update client2.py

--- client2.py
+++ client2.py
@@ -10,7 +10,9 @@ printer2 = Demo.PrinterPrx.checkedCast(base2)
 if (not printer1) or (not printer2):
     raise RuntimeError("Invalid proxy")
 
-printer1.printString("Hello World from printer1!")
-printer2.printString("Hello World from printer2!")
+rep = printer1.printString("Hello World from printer1!")
+print(rep)
+rep = printer2.printString("Hello World from printer2!")
+print(rep)
 
 communicator.waitForShutdown()
